29a8e64b7e413ae1ee738959243323288d0fdefb
Mario Hsieh <mariohsieh@gmail.com>
2014-07-09 05:37:43 -0700

    added artwork, gallery function

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
 	}
 
 	// event handlers
+	
+	// main navigation click
 	$(document).on("click", ".container nav li", function() {
 		var pagelink = $(this).html();
 		$(".container nav li").removeClass("active");
@@ -14,7 +16,17 @@ $(document).ready(function() {
 		$('#'+pagelink).css("display", "inherit");
 	});
 
-
+	// artwork click
+	$(document).on("click", "#artwork img", function() {
+		var imageName = $(this).attr("alt");
+		$(".overlay").css("display", "block").append("<img alt='"+imageName+"' src='img/"+imageName+".jpg' />");
+	});
+	
+	// exit from gallery
+	$(document).on("click", ".overlay", function() {
+		$(this).css("display", "none").empty();
+	});
+		
 	initial();
 });
 
